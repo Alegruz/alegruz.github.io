41c91214ababc418685f04b379d6b98a2dc36141
Alegruz <alegruz@khu.ac.kr>
2025-07-14 02:03:44 +0900

    [UPDATE] Refactor display buffer management and improve resolution handling in raytracing module

--- assets/codes/raytracing/main.cpp
+++ assets/codes/raytracing/main.cpp
@@ -4,49 +4,31 @@
 
 #include "math.h"
 
+static constexpr uint32_t MAX_WIDTH = 1920;
+static constexpr uint32_t MAX_HEIGHT = 1080;
+static constexpr uint32_t CHANNELS_COUNT = 4; // RGBA
+static constexpr uint32_t NUM_COLORS = 256;
 static uint32_t sWidth = 1280;
 static uint32_t sHeight = 720;
-static uint8_t* sPixels = nullptr;
+static uint8_t sPixels[MAX_WIDTH * MAX_HEIGHT * CHANNELS_COUNT];
 
 void clear_display_buffer();
-void initialize_display_buffer();
 
 extern "C" EMSCRIPTEN_KEEPALIVE
 void set_resolution(const uint32_t width, const uint32_t height)
 {
-    sWidth = width;
-    sHeight = height;
-
-    if(sPixels != nullptr)
-    {
-        delete[] sPixels;
-        sPixels = nullptr;
-    }
-
-    initialize_display_buffer();
+    sWidth = std::clamp(width, 1u, MAX_WIDTH);
+    sHeight = std::clamp(height, 1u, MAX_HEIGHT);
 }
 
 void clear_display_buffer()
 {
-    if(sPixels != nullptr)
-    {
-        std::memset(sPixels, 0, sWidth * sHeight * 4);
-    }
-}
-
-void initialize_display_buffer()
-{
-    if(sPixels == nullptr)
-    {
-        sPixels = new uint8_t[sWidth * sHeight * 4];
-    }
+    std::memset(sPixels, 0, sWidth * sHeight * CHANNELS_COUNT);
 }
 
 extern "C" EMSCRIPTEN_KEEPALIVE
 void render_frame(const uint32_t frameIndex)
 {
-    initialize_display_buffer();
-
     // Clear the pixel buffer
     clear_display_buffer();
 
@@ -55,11 +37,11 @@ void render_frame(const uint32_t frameIndex)
     {
         for(uint32_t x = 0; x < sWidth; ++x)
         {
-            uint32_t index = (y * sWidth + x) * 4;
-            sPixels[index] = 255 - (frameIndex % 255);     // Red
+            uint32_t index = (y * sWidth + x) * CHANNELS_COUNT;
+            sPixels[index] = NUM_COLORS - 1 - (frameIndex % (NUM_COLORS - 1));     // Red
             sPixels[index + 1] = 0;   // Green
             sPixels[index + 2] = 0;   // Blue
-            sPixels[index + 3] = 255; // Alpha
+            sPixels[index + 3] = (NUM_COLORS - 1); // Alpha
         }
     }
 }
@@ -67,6 +49,5 @@ void render_frame(const uint32_t frameIndex)
 extern "C" EMSCRIPTEN_KEEPALIVE
 uint8_t* get_display_buffer()
 {
-    initialize_display_buffer();
     return sPixels;
 }
